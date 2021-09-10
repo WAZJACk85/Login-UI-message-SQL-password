8dd5f3bd745da7d536f25c10175b4e3d82b96f69
Warren Humphreys <warrenhumphreys@gmail.com>
2021-09-10 20:59:56 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace Login_Demo_Shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Picture1 = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.RichTextBox();
+            this.txtPassword = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(97, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 465);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(291, 270);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(51, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(819, 156);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Picture1
+            // 
+            this.Picture1.Image = global::Login_Demo_Shop.Properties.Resources.Baab_thai_rest;
+            this.Picture1.Location = new System.Drawing.Point(381, 304);
+            this.Picture1.Name = "Picture1";
+            this.Picture1.Size = new System.Drawing.Size(125, 62);
+            this.Picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Picture1.TabIndex = 1;
+            this.Picture1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.AccessibleName = "btnLogin";
+            this.btnLogin.Location = new System.Drawing.Point(397, 607);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(94, 29);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnLogin_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.AccessibleName = "txtUsername1";
+            this.txtUsername.Location = new System.Drawing.Point(273, 440);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(334, 38);
+            this.txtUsername.TabIndex = 3;
+            this.txtUsername.Text = "Username \\ Email";
+            this.txtUsername.Enter += new System.EventHandler(this.txtUserEnter);
+            this.txtUsername.Leave += new System.EventHandler(this.txtUserLeave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AccessibleName = "txtPassword1";
+            this.txtPassword.Location = new System.Drawing.Point(273, 502);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(334, 38);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassEnter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 785);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.Picture1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        
+
+
+
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox Picture1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.RichTextBox txtUsername;
+        private System.Windows.Forms.RichTextBox txtPassword;
+        
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Login_Demo_Shop
+{
+
+    public partial class Form1 : Form
+    {
+    SqlConnection con = new SqlConnection();
+    SqlCommand com = new SqlCommand();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            con.ConnectionString = @"Data Source = LAPTOP-LL7MF4G0\MSSQL_APPOINT; Initial Catalog = westlake; Integrated Security = True";
+        }
+
+        
+        private void txtUserEnter(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Equals(@"Username \ Email"))
+            {
+                txtUsername.Text = "";
+            }
+
+        }
+
+        private void txtUserLeave(object sender, EventArgs e)
+        {
+
+            if (txtUsername.Text.Equals(""))
+            {
+                txtUsername.Text = @"Username \ Email";
+            }
+
+        }
+
+        private void txtPassEnter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text.Equals("Password"))
+            {
+                txtPassword.Text = "";
+            }
+
+        }
+
+        private void txtPassLeave(object sender, EventArgs e)
+        {
+
+            if (txtPassword.Text.Equals(""))
+            {
+                txtPassword.Text = ("Password");
+            }
+
+
+
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+             
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from AUTH";
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                if (txtUsername.Text.Equals(dr["username"].ToString()) && txtPassword.Text.Equals(dr["password"].ToString()))
+                {
+                    MessageBox.Show("Login Successfull", "Well Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Password or Username is incorrect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            con.Close();
+
+
+        }
+    }   
+}
